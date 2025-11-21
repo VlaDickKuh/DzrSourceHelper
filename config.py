12bc8d7bae8487145eb9469eba7b978b107b568f
Vladick <vladick.kuharev@yandex.ru>
2025-11-21 06:59:44 +0300

    Update config file

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import requests
 
@@ -7,15 +8,28 @@ load_dotenv()
 
 
 
-LOGIN = os.getenv("LOGIN")
-PASSWORD = os.getenv("PASSWORD")
+# LOGIN = os.getenv("LOGIN")
+# PASSWORD = os.getenv("PASSWORD")
 
-GAME_ID = os.getenv("GAME_ID")
-CITY = os.getenv("CITY")
+# GAME_ID = os.getenv("GAME_ID")
+# CITY = os.getenv("CITY")
+# S_URL = f"https://classic.dzzzr.ru/{CITY}/admin/admin.php"
+# FILES_UPLOAD_URL = f"https://classic.dzzzr.ru/{CITY}/admin/tinymce/jscripts/tiny_mce/plugins/filemanager/stream/index.php"
+# DOCUMENT_ID = os.getenv("DOCUMENT_ID")
+# FOLDER_ID = os.getenv("FOLDER_ID")
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+LOGIN = config.get("login")
+PASSWORD = config.get("password")
+
+GAME_ID = config.get("game_id")
+CITY = config.get("city")
 S_URL = f"https://classic.dzzzr.ru/{CITY}/admin/admin.php"
 FILES_UPLOAD_URL = f"https://classic.dzzzr.ru/{CITY}/admin/tinymce/jscripts/tiny_mce/plugins/filemanager/stream/index.php"
-DOCUMENT_ID = os.getenv("DOCUMENT_ID")
-FOLDER_ID = os.getenv("FOLDER_ID")
+DOCUMENT_ID = config.get("document_id")
+FOLDER_ID = config.get("folder_id")
 
 def get_session():
     session = requests.Session()
